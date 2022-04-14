3ba08f8e7c41a4199e740acc4b4336b0d829df53
55emon87 <ashikuremon5@gmail.com>
2022-04-14 22:33:21 +0600

    second

--- src/App.js
+++ src/App.js
@@ -1,10 +1,56 @@
-import logo from './logo.svg';
+import { getAuth } from "firebase/auth";
 import './App.css';
+import app from "./firebase.init";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+
+
+// const auth = getAuth(app);
+const auth = getAuth (app);
+
+
 
 function App() {
+const handleEmail = e =>{
+  console.log(e.target.value)
+}
+
+const handlePassword =e=>{
+  console.log(e.target.value)
+}
+const handleSubmit = e=>{
+  console.log('submitted')
+  e.preventDefault ();
+}
+
+
   return (
-    <div className="App">
-      
+    <div >
+    <div className="registration w-50 mx-auto ">
+      <h2 className="text-primary" > Please Registration </h2>
+    <Form>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
+    </div>
     </div>
   );
 }
---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDNw1gz0AH3Vab11Bj0OWyqDENxqqJWccU",
+  authDomain: "simple-login-authenticat-e5da1.firebaseapp.com",
+  projectId: "simple-login-authenticat-e5da1",
+  storageBucket: "simple-login-authenticat-e5da1.appspot.com",
+  messagingSenderId: "853216959525",
+  appId: "1:853216959525:web:bf1feee0e5191e9d06f3cc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
